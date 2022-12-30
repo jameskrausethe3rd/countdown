44de8614119ab541050552af62ab587c11ebbb7d
James Krause <102771123+jameskrausethe3rd@users.noreply.github.com>
2022-12-29 22:08:15 -0600

    Begin Angular Dev

---
+++ countdown/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TimerElementComponent } from './timer-element/timer-element.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TimerElementComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ countdown/src/app/timer-element/timer-element.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer-element',
+  templateUrl: './timer-element.component.html',
+  styleUrls: ['./timer-element.component.sass']
+})
+export class TimerElementComponent{
+  title = 'TESTING';
+}
